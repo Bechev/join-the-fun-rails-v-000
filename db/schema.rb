b2bb3f50c02880baf26042f37f20f5e64931e3cb
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-06-14 01:20:30 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180614011433) do
+ActiveRecord::Schema.define(version: 20140212205318) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20180614011433) do
   create_table "taxis", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "ride_id"
   end
 
 end
